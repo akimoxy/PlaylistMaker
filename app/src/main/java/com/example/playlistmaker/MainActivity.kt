fc980bcb7214d15f8efddb3ecb277645b6559acf
AkimOxy <akimova.oki@gmail.com>
2023-11-24 19:54:11 +0300

    спринт 9

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonSearch = findViewById<Button>(R.id.search_button)
+        val buttonSearch = findViewById<Button>(R.id.search_button_main)
 
         buttonSearch.setOnClickListener {
             val buttonSearchIntent = Intent(this, SearchActivity::class.java)
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,76 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val buttonBackInSettings = findViewById<Button>(R.id.back_button_search)
+        buttonBackInSettings.setOnClickListener {
+            finish()
+        }
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        val clearButton = findViewById<Button>(R.id.clear_icon_search)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+        fun clearButtonVisibility(s: CharSequence?): Int {
+            return if (s.isNullOrEmpty()) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        val text = inputEditText.text.toString()
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY, text)
+    }
+
+    companion object {
+        const val KEY = "someKey"
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        val text = savedInstanceState.getString(KEY)
+        inputEditText.setText(text)
     }
 }
+
+
+
+
+
+
+
